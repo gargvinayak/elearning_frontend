793f5beb3f4e2b4667a05b1a1c85953d4f0465ac
gargvinayak <137904027+gargvinayak@users.noreply.github.com>
2024-06-24 14:19:19 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {UserContextProvider} from "./context/UserContext.jsx"
 import {CourseContextProvider} from "./context/CourseContext.jsx"
-export const server = "http://localhost:4000";
+export const server = "https://elearning-server-o5np.onrender.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
